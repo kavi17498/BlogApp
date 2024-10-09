9c746ae4619bd7c6912b6d9bd81beb065e645b5e
Kavi17498 <kavindulakshan187@gmail.com>
2024-10-09 18:00:14 +0530

    start

---
+++ client/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import PostDetails from './pages/PostDetails';
+import Register from './pages/Register';
+import Login from './pages/Login'
+import UserProfile from './pages/UserProfile';
+import Authors from './pages/Authors';
+import CreatePost from './pages/CreatePost';
+import CategoryPosts from './pages/CategoryPosts';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    errorElement: <ErrorPage />,
+    children:[
+      {index:true ,element: <Home />},
+      {path:"posts/:id",element: <PostDetails />},
+      {path:"register",element: <Register />},
+      {path:"login",element: <Login />},
+      {path:"profile/:id",element: <UserProfile />},
+      {path:"authors",element: <Authors />},
+      {path:"create",element: <CreatePost />},
+      {path:"posts/categories/:category",element: <CategoryPosts />},
+      
+
+    ]
+  }
+])
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
---
+++ client/src/pages/Deletepost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Deletepost() {
+  return (
+    <div>Deletepost</div>
+  )
+}
+
+export default Deletepost
