96cc5b2b0918998842fe519d1107725e3570b308
Kavi17498 <kavindulakshan187@gmail.com>
2024-10-09 22:47:58 +0530

    navbar done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
+import Image from "../images/girl.jpg"
+import {FaBars} from "react-icons/fa"
+import {AiOutlineClose} from "react-icons/ai"
 function Header() {
   return (
-    <div>Header</div>
+    <nav>
+      <div className='container nav__container'>
+      <Link to="/" className='nav__logo'>
+      <img src={Image} alt='camerawom' ></img>
+      </Link>
+      <ul className='nav-menu'>
+        <li> <Link to="/profile">Profile</Link> </li>
+        <li> <Link to="/create">Create post</Link> </li>
+        <li> <Link to="/authors">Authors</Link> </li>
+        <li> <Link to="/logout">Logout</Link> </li>
+      </ul>
+      <button className='nav__toggle-btn'>
+        <AiOutlineClose />
+      </button>
+      </div>
+          </nav>
   )
 }
 
